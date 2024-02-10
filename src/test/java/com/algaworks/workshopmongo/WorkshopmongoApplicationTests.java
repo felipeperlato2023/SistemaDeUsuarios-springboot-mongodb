314fd88ac3dc2c7a5e37a18ce0661756914f103d
Felipe Perlato <fe.perlato@gmail.com>
2024-02-10 16:21:40 -0300

    criação do projeto

---
+++ src/test/java/com/algaworks/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algaworks.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
