57c90996ec091b8e039fc0578518b952152f0f94
Felipe Perlato <fe.perlato@gmail.com>
2024-02-10 17:31:23 -0300

    conectando ao MongoDB com repository e service

--- src/main/java/com/algaworks/workshopmongo/Resources/userResource.java
+++ src/main/java/com/algaworks/workshopmongo/Resources/userResource.java
@@ -1,30 +1,27 @@
 package com.algaworks.workshopmongo.Resources;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.algaworks.workshopmongo.Services.UserService;
 import com.algaworks.workshopmongo.domain.User;
 
 @RestController
 @RequestMapping(value = "/users")
-public class userResource {
+public class UserResource {
+
+	@Autowired
+	private UserService service;
 
 	@GetMapping
 	public ResponseEntity<List<User>> findAll() {
 
-		User maria = new User("1", "Maria Brown", "Maria@gmail.com");
-		User alex = new User("2", "Alex Brown", "Alex@gmail.com");
-
-		List<User> list = new ArrayList<>();
-
-		list.add(alex);
-		list.add(maria);
-
+		List<User> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 
---
+++ src/main/java/com/algaworks/workshopmongo/Services/UserService.java
@@ -0,0 +1,21 @@
+package com.algaworks.workshopmongo.Services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.algaworks.workshopmongo.Repositories.UserRepository;
+import com.algaworks.workshopmongo.domain.User;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+
+}
