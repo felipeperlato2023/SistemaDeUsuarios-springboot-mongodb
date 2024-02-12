8eac906f2d0bc5bbfc6d1cd8ec076f9476bc731a
Felipe Perlato <fe.perlato@gmail.com>
2024-02-12 11:33:28 -0300

    user dto

---
+++ src/main/java/com/algaworks/workshopmongo/Dtos/userDTO.java
@@ -0,0 +1,70 @@
+package com.algaworks.workshopmongo.Dtos;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import com.algaworks.workshopmongo.domain.User;
+
+
+public class userDTO implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+	private String id;
+	private String name;
+	private String email;
+
+	public userDTO() {
+		super();
+	}
+
+	public userDTO(User obj) {
+		super();
+		this.id = obj.getId();
+		this.name = obj.getName();
+		this.email = obj.getEmail();
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		userDTO other = (userDTO) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+
+}
--- src/main/java/com/algaworks/workshopmongo/Resources/userResource.java
+++ src/main/java/com/algaworks/workshopmongo/Resources/userResource.java
@@ -1,6 +1,7 @@
 package com.algaworks.workshopmongo.Resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.algaworks.workshopmongo.Dtos.userDTO;
 import com.algaworks.workshopmongo.Services.UserService;
 import com.algaworks.workshopmongo.domain.User;
 
@@ -19,10 +21,12 @@ public class UserResource {
 	private UserService service;
 
 	@GetMapping
-	public ResponseEntity<List<User>> findAll() {
+	public ResponseEntity<List<userDTO>> findAll() {
 
 		List<User> list = service.findAll();
-		return ResponseEntity.ok().body(list);
+		
+		List<userDTO> listDto = list.stream().map(x -> new userDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
 
 }
